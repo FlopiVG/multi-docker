06c5354b43a8b7b5c4a45516a0797d1c3b7aa3a2
Flopi <flopivillagarcia@gmail.com>
2019-06-12 23:30:33 +0200

    Validate index required

--- server/index.js
+++ server/index.js
@@ -49,7 +49,9 @@ app.get("/values/current", async (req, res) => {
 app.post("/values", async (req, res) => {
   const index = req.body.index;
 
-  if (parseInt(index) > 40) {
+  if (!index) {
+    return res.send(400).send("Index must be required");
+  } else if (parseInt(index) > 40) {
     return res.status(422).send("Index too high");
   }
 
